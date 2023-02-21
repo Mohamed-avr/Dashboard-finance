124a20dc4a7cf8acf94c9981ebb8b17aefc8ebb4
boukhanouf mohamed <mouh4945@gmail.com>
2023-02-21 12:45:46 +0100

    ADD

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Layout from 'Components/Layout'
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+
+   const [resposnive , setResponsive] = useState(false);
   return (
     <>
      <Layout>
