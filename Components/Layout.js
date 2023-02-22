b1262b4826ca94ab77452fa3035fde973b26a8fa
boukhanouf mohamed <mouh4945@gmail.com>
2023-02-22 01:02:50 +0100

    ADD

--- Components/Layout.js
+++ Components/Layout.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from './Navbar';
 
-const Layout = ( {children}) => {
+const Layout = (  { children }) => {
+
+      
+      
     return (
         <div>
             {/* header  */}
             <Navbar/>
             {children}
+          
 
             {/* footer   */}
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Layout from 'Components/Layout'
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,6 +9,17 @@ export default function Home() {
 
 
    const [resposnive , setResponsive] = useState(false);
+
+
+  useEffect(() => {
+    if(window.innerWidth > 600) {
+      setResponsive(true) 
+    } else {
+      setResponsive(false)
+    }
+   console.log(resposnive)
+  }, [setResponsive]);
+     
   return (
     <>
      <Layout>
