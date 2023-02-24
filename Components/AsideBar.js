7b449c662693e0ee03246212629cfe9d9ebf5972
boukhanouf mohamed <mouh4945@gmail.com>
2023-02-24 13:09:03 +0100

    FIX

--- Components/AsideBar.js
+++ Components/AsideBar.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import Image from "next/image";
+
 
 const AsideBar = () => {
     return (
        <aside className='fixed left-0 top-[52px] bg-sidebar w-[10rem] h-full '>
+          <div className=''>
+                <Image src={'/logo.png'} alt={'logo'} width={40} height={20}  />
+          </div>
 
        </aside>
     );
