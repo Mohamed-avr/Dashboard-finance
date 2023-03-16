4b17277953a43151bf42ee7861a36531958d65d6
boukhanouf mohamed <mouh4945@gmail.com>
2023-03-16 19:05:12 +0100

    Payment component - dashboard

--- Components/content/Dashboard.js
+++ Components/content/Dashboard.js
@@ -1,6 +1,7 @@
 import MoneyState from 'Components/MoneyState';
 import { useContext } from 'react';
 import AppContext from 'Components/AppContext';
+import Payment from './Payment';
 
 
 
@@ -10,16 +11,15 @@ const Dashboard = () => {
     console.log(MoneyStateData)
     return (
         <section className='sm:m-10   '>
-            <div className=' flex sm:justify-start justify-center  items-center  flex-wrap  mt-8 sm:mt-0 '>
 
+            <div className=' flex sm:justify-start justify-center  items-center  flex-wrap  mt-8 sm:mt-0 '>
                 {MoneyStateData.map((e, i) => {
                     return (<MoneyState key={i} sendTitle={e.title} sendIcon={e.icon} sendMoney={e.money} />)
                 })}
             </div>
 
-            <section className='flex sm:flex-row items-center  justify-center sm:items-start   flex-col sm:space-x-4 mt-4 w-full '>
-                <div className=' sm:pl-2 flex flex-wrap space-y-4  pl-0 justify-center '>
-
+            <section className='flex sm:flex-row items-center  justify-center sm:items-start flex-col sm:space-x-4 mt-4 w-full'>
+                <div className=' sm:pl-2 flex flex-wrap space-y-4  pl-0 sm:justify-start justify-center '>
                     <article className='sm:w-[38rem] w-[19rem] h-[20rem] bg-sidebar rounded-lg '>
                         <div className='flex justify-between items-center text-lightColor px-5 py-3 '>
                             <div className='capitalize text-[#fff]  sm:text-xl text-xs'> overview </div>
@@ -36,12 +36,16 @@ const Dashboard = () => {
                     </article>
 
 
-                    <div className='flex justify-start sm:items-center sm:flex-row flex-col  sm:space-x-2  sm:space-y-0 space-y-4  '>
+                    <div className='flex justify-start sm:items-start sm:flex-row flex-col  sm:space-x-2  sm:space-y-0 space-y-4  '>
                         <article className='w-[19rem] h-40 bg-sidebar rounded-lg  '>
                             chart   lines
                         </article>
-                        <article className='w-[19rem] h-40 bg-sidebar rounded-lg  '>
-                            chart
+                        <article className='w-[19rem] h-auto bg-sidebar rounded-lg py-4 '>
+                             <h2  className='ml-4 font-medium text-white'> Payment </h2>
+                            <Payment/>
+                            <Payment/>
+                            <Payment/>
+                            <Payment/>
                         </article>
 
                     </div>
---
+++ Components/content/Payment.js
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+
+const Payment = () => {
+    return (
+        <div className="flex  justify-center items-center flex-row mt-4  ">
+            <div className="w-10 h-10 bg-body rounded-lg flex justify-center items-center ">
+                <Image src={'/Icon/message.svg'} alt={'icon-desc'} width={24} height={24} />
+            </div>
+            <div className="flex flex-col w-[15rem] pl-2 -mt-2">
+                <div className="  flex justify-between px-2 my-1">
+                     <h4 className=" text-sm "> account </h4> 
+                     <h5 className=" text-sm  " > 4011/4400</h5>
+                     </div>
+                <div>
+                     <div className="w-[14rem] bg-body h-2 rounded-md overflow-hidden absolute ">
+                    <span className="h-2 bg-selectedColor absolute left-0 top-0 w-4/6 "> </span>
+                </div> 
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Payment;
