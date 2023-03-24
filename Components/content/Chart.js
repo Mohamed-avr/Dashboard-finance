06208dbfb1d10dc94158e75ae1c36369934fbc60
boukhanouf mohamed <mouh4945@gmail.com>
2023-03-24 18:08:53 +0100

    Charts - Dashbord component

---
+++ Components/content/Chart.js
@@ -0,0 +1,42 @@
+import Image from "next/image";
+
+const Chart = () => {
+    return (
+        <div className='flex justify-between mt-5  w-full flex-row px-4'>
+            <div className="space-y-8 text-xs -mt-2 text-lightColor ">
+                <h5> $2000 </h5>
+                <h5> $1500 </h5>
+                <h5> $1000 </h5>
+                <h5> $500 </h5>
+            </div>
+
+            <div className=" flex flex-col relative ">
+               <div className="relative  ">
+                <button className=" top-2/4 left-2/4 -ml-[40%] -mt-[10%] absolute text-left bg-selectedColor hover:bg-[#29af4f] rounded-[8px] py-2 pl-3 pr-5 ">
+                    <h3 className="text-xs font-thin text-[#fff]">income</h3>
+                    <h5 className="text-sm font-medium text-[#fff]">224.00</h5>
+                    
+                </button>
+                   <Image  src={'/Charts.png'} width={530} height={300} alt={"charts"} />
+               </div>
+         <div className="flex space-x-6 text-xs mt-2 text-lightColor">
+                <h5> jan </h5>
+                <h5> Feb </h5>
+                <h5> Mar </h5>
+                <h5> Apr </h5>
+                <h5> May </h5>
+                <h5> jun </h5>
+                <h5> jul </h5>
+                <h5> Aug </h5>
+                <h5> Sep </h5>
+                <h5> Oct </h5>
+                <h5> Nov </h5>
+                <h5> Dec </h5>
+         </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Chart;
--- Components/content/Dashboard.js
+++ Components/content/Dashboard.js
@@ -2,13 +2,17 @@ import MoneyState from 'Components/MoneyState';
 import { useContext } from 'react';
 import AppContext from 'Components/AppContext';
 import Payment from './Payment';
+import ChartElement from 'public/chartElement';
+import Chart from './Chart';
 
+// ChartElement
 
 
 const Dashboard = () => {
     const { MoneyStateData } = useContext(AppContext);
-   
-    
+
+
+
 
     return (
         <section className='sm:m-10   '>
@@ -24,20 +28,20 @@ const Dashboard = () => {
                     <article className='sm:w-[38rem] w-[19rem] h-[20rem] bg-sidebar rounded-lg '>
                         <div className='flex justify-between items-center text-lightColor px-5 py-3 '>
                             <div className='capitalize text-[#fff]  sm:text-xl text-xs'> overview </div>
-                            <div className='flex space-x-4 items-center  '> 
+                            <div className='flex space-x-4 items-center  '>
                                 <div className='text-sm sm:flex items-center hidden'> <div className='w-[10px] h-[10px] rounded-full bg-selectedColor mr-1'> </div> incume </div>
                                 <div className='text-sm sm:flex items-center hidden '> <div className='w-[10px] h-[10px] rounded-full bg-[#FFC01E]  mr-1'> </div> expenses </div>
                                 <select className=' bg-transparent hover:border-selectedColor hover:shadow-selectedColor/40   rounded-lg py-2 px-1 bg-[#fff]/0 border outline-none text-sm cursor-pointer text-lightColor'>
-                                    <option className='bg-[#fff]/0 text-selectedColor ' value={'monthly'}>monthly</option> 
+                                    <option className='bg-[#fff]/0 text-selectedColor ' value={'monthly'}>monthly</option>
                                     <option value={'monthly'}>weekly </option>
                                     <option value={'monthly'}>daily </option>
                                 </select>
                             </div>
-                            
+
                         </div>
-                        <div className=' bg-selectedColor '> 
-                                hello
-                            </div>
+                        <div className='   h-[30rem] '>
+                            <Chart/>
+                        </div>
                     </article>
 
 
@@ -46,12 +50,12 @@ const Dashboard = () => {
                             chart   lines
                         </article>
                         <article className='w-[19rem] h-auto bg-sidebar rounded-lg py-4 pb-5 '>
-                             <h2  className='ml-4 font-medium text-white text-lightColor'> Payment </h2>  
-                                                           {/*  js code - that appears the payments content */}
-                            <Payment/>
-                            <Payment/>
-                            <Payment/>
-                            <Payment/>
+                            <h2 className='ml-4 font-medium text-white text-lightColor'> Payment </h2>
+                            {/*  js code - that appears the payments content */}
+                            <Payment />
+                            <Payment />
+                            <Payment />
+                            <Payment />
                         </article>
 
                     </div>
